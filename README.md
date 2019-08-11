c4df35cbb6d3e3103fc0639c4bfd18cf6749e65a
Diego Gurgel <hi@diegogurgel.com>
2019-08-11 15:47:42 +0200

    Add loading status and running instructions

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # WebRTC with react
-This is a simple 1-to-1 video chat room example using react with webRTC. This is experimental for learnig purposes. I hope that this project help you to learn something. You can test this on https://video-chat.now.sh put a room id that you want to create and click enter the other peer have to use the same room id. Sometimes it takes a bit longer to connect(no longer than one minute).     
 
+This is a simple 1-to-1 video chat room example using react with webRTC. This is experimental for learnig purposes. I hope that this project help you to learn something. You can test this on https://video-chat.now.sh put a room id that you want to create and click enter the other peer have to use the same room id. Sometimes it takes a bit longer to connect(no longer than one minute).
 
-To run this application you need to create a TURN account. You can create one using this service http://numb.viagenie.ca/cgi-bin/numbacct 
+### Running locally
+
+To run this application you need to create a TURN account. You can create one using this service http://numb.viagenie.ca/cgi-bin/numbacct
 
 Change the `.env.example` to `.env` or `.env.local`
 
 Put your turn account on the .env file
 
+#### The folders
+
+Front-end files are inside `/src` and the server files are inside `/server` folder
+
+#### Install the dependencies
+
+```shell
+npm i
+```
+
+#### Start the application
+
+```shell
+npm run start:local
+```
--- src/components/video.js
+++ src/components/video.js
@@ -3,100 +3,133 @@ import VideoCall from '../helpers/simple-peer'
 import '../styles/video.css'
 import io from 'socket.io-client'
 
-class Video extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            localStream:{},
-            remoteStreamUrl:'',
-            streamUrl:'',
-            initiator: false,
-            peer:{},
-            full:false,
-        }
-
+class Video extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      localStream: {},
+      remoteStreamUrl: '',
+      streamUrl: '',
+      initiator: false,
+      peer: {},
+      full: false,
+      connecting: false,
+      waiting: true
     }
-    videoCall = new VideoCall()
-    componentDidMount(){
-        const socket = io(process.env.REACT_APP_SIGNALING_SERVER)
-        const component = this
-        this.setState({socket})
-        const { roomId } = this.props.match.params
-        this.getUserMedia().then(() => {
-            socket.emit('join', { roomId:roomId })
-        })
-        socket.on('init', ()=>{
-            component.setState({ initiator: true})
-        })
-        socket.on('ready', ()=>{
-            component.enter(roomId)
-        })
-        socket.on('desc', (data) => {
-            if (data.type ==='offer' && component.state.initiator) return
-            if (data.type === 'answer' && !component.state.initiator) return
-            component.call(data) 
-        })
-        socket.on('disconnected', () => {
-            component.setState({ initiator: true })
-        })
-        socket.on('full', () => {
-            component.setState({full:true})
-        })
-
+  }
+  videoCall = new VideoCall()
+  componentDidMount() {
+    const socket = io(process.env.REACT_APP_SIGNALING_SERVER)
+    const component = this
+    this.setState({ socket })
+    const { roomId } = this.props.match.params
+    this.getUserMedia().then(() => {
+      socket.emit('join', { roomId: roomId })
+    })
+    socket.on('init', () => {
+      component.setState({ initiator: true })
+    })
+    socket.on('ready', () => {
+      component.enter(roomId)
+    })
+    socket.on('desc', data => {
+      if (data.type === 'offer' && component.state.initiator) return
+      if (data.type === 'answer' && !component.state.initiator) return
+      component.call(data)
+    })
+    socket.on('disconnected', () => {
+      component.setState({ initiator: true })
+    })
+    socket.on('full', () => {
+      component.setState({ full: true })
+    })
+  }
+  getUserMedia(cb) {
+    return new Promise((resolve, reject) => {
+      navigator.getUserMedia = navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia
+      const op = {
+        video: {
+          width: { min: 160, ideal: 640, max: 1280 },
+          height: { min: 120, ideal: 360, max: 720 }
+        },
+        audio: true
+      }
+      navigator.getUserMedia(
+        op,
+        stream => {
+          this.setState({ streamUrl: stream, localStream: stream })
+          this.localVideo.srcObject = stream
+          resolve()
+        },
+        () => {}
+      )
+    })
+  }
+  enter = roomId => {
+    this.setState({ connecting: true })
+    const peer = this.videoCall.init(
+      this.state.localStream,
+      this.state.initiator
+    )
+    peer.on('signal', data => {
+      const signal = {
+        room: roomId,
+        desc: data
+      }
+      this.state.socket.emit('signal', signal)
+    })
+    peer.on('stream', stream => {
+      this.remoteVideo.srcObject = stream
+      this.setState({ connecting: false, waiting: false })
+    })
+    peer.on('error', function(err) {
+      console.log(err)
+    })
+  }
+  call = otherId => {
+    this.videoCall.connect(otherId)
+  }
+  renderFull = () => {
+    if (this.state.full) {
+      return 'The room is full'
     }
-    getUserMedia(cb){
-        return new Promise((resolve, reject) => {
-            navigator.getUserMedia = navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-            const op = {
-                video: {
-                    width: { min: 160, ideal: 640, max: 1280 },
-                    height: { min: 120, ideal: 360, max: 720 },
-                },
-                audio: true 
-            }
-            navigator.getUserMedia(op, stream => {
-                this.setState({ streamUrl: stream, localStream: stream })
-                this.localVideo.srcObject = stream
-                resolve()
-            }, () => { }) 
-        })
-    }
-    enter = (roomId) => {
-        const peer = this.videoCall.init(this.state.localStream, this.state.initiator)
-        peer.on('signal', data => {
-            const signal = {
-                room: roomId,
-                desc: data
-            }
-            this.state.socket.emit('signal', signal)
-        })
-        peer.on('stream', stream => {
-            this.remoteVideo.srcObject = stream
-        })
-        peer.on('error', function (err) { 
-            console.log(err)
-        })
-
-    }
-    call = (otherId) => {
-        this.videoCall.connect(otherId)
-    }
-    renderFull = () => {
-        if(this.state.full){
-            return 'The room is full'
-        }
-    }
-    render(){
-        return( 
-            <div className="video-wrapper">
-                <div className="local-video-wrapper">
-                    <video autoPlay id="localVideo" muted ref={video => (this.localVideo = video)}></video>
-                </div>
-                <video autoPlay id="remoteVideo"  ref={video => (this.remoteVideo = video)}></video>
-                {this.renderFull()}
-            </div>
-        )
-    }
+  }
+  render() {
+    return (
+      <div className="video-wrapper">
+        <div className="local-video-wrapper">
+          <video
+            autoPlay
+            id="localVideo"
+            muted
+            ref={video => (this.localVideo = video)}
+          />
+        </div>
+        <video
+          autoPlay
+          className={`${
+            this.state.connecting || this.state.waiting ? 'hide' : ''
+          }`}
+          id="remoteVideo"
+          ref={video => (this.remoteVideo = video)}
+        />
+        {this.state.connecting && (
+          <div className="status">
+            <p>Establishing connection...</p>
+          </div>
+        )}
+        {this.state.waiting && (
+          <div className="status">
+            <p>Waiting for someone...</p>
+          </div>
+        )}
+        {this.renderFull()}
+      </div>
+    )
+  }
 }
 
 export default Video
