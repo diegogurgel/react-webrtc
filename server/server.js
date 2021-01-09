4fb6c4ef7789664cbbf9b8bf6eeab3d3c1f7187b
Diego Gurgel <hi@diegogurgel.com>
2021-01-09 19:24:00 +0100

    confirgure default port to be 80

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(8080);
+server.listen(process.env.PORT || 80);
 
 io.on('connection', function (socket) {
     socket.on('join', function (data) {
